10423d23fe8bc8aa3805901996d07e4dd5f48b60
Chan Kang <chankang17@gmail.com>
2024-10-15 18:40:06 +0900

    add e2e/*

---
+++ e2e/two_revisions_on_head/migrations/versions/1673531f822c_.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: 1673531f822c
+Revises: 
+Create Date: 2024-10-15 18:39:31.140679
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '1673531f822c'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
